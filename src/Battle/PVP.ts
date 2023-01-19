8feb68326613bfbf53d3656cf3367befa8d2682d
lucasdasilva <xBigodinho@outlook.com>
2023-01-19 07:26:24 -0300

    feat: create PVP class and refactore Battle class

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,26 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+const LOST_BATTLE = -1;
+
+class PVP extends Battle {
+  constructor(
+    private _firstCharacter: Fighter,
+    private _secondCharacter: Fighter,
+  ) {
+    super();
+  }
+
+  fight(): number {
+    while (
+      this._firstCharacter.lifePoints !== LOST_BATTLE
+      && this._secondCharacter.lifePoints !== LOST_BATTLE
+    ) {
+      this._firstCharacter.attack(this._secondCharacter);
+      this._secondCharacter.attack(this._firstCharacter);
+    }
+    return this._firstCharacter.lifePoints === LOST_BATTLE ? -1 : 1;
+  }
+}
+
+export default PVP;
