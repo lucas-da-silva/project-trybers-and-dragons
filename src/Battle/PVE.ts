8b0b133451ae52a6c7c8771e0a2bcc270fd1bc46
lucasdasilva <xBigodinho@outlook.com>
2023-01-19 08:34:36 -0300

    feat: created PVE class

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,28 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+const LOST_BATTLE = -1;
+
+class PVE extends Battle {
+  constructor(
+    private _character: Fighter,
+    private _monsters: (SimpleFighter | Fighter)[],
+  ) {
+    super();
+  }
+
+  fight(): number {
+    while (
+      this._character.lifePoints !== LOST_BATTLE
+      && this._monsters.every(({ lifePoints }) => lifePoints !== LOST_BATTLE)
+    ) {
+      this._monsters.forEach((monster) => {
+        this._character.attack(monster);
+        monster.attack(this._character);
+      });
+    }
+    return this._character.lifePoints === LOST_BATTLE ? -1 : 1;
+  }
+}
+
+export default PVE;
